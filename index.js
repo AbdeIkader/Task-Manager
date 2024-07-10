d5c59339e50ead68f8fe857d336478afda4df23c
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2024-07-10 18:38:28 +0300

    DB Setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from 'express'
+import { dbConnection } from "./DB/dbConnection.js";
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-
+dbConnection()
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
